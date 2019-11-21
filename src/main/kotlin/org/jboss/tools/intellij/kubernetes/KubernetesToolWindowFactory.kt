ebe48c614fed1df31684b85ce5771b6fcb2680c8
Andre Dietisheim <adietish@redhat.com>
2019-11-21 15:59:24 +0100

    impl'd loading of namespaces
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/kotlin/org/jboss/tools/intellij/kubernetes/KubernetesToolWindowFactory.kt
+++ src/main/kotlin/org/jboss/tools/intellij/kubernetes/KubernetesToolWindowFactory.kt
@@ -10,19 +10,30 @@
  ******************************************************************************/
 package org.jboss.tools.intellij.kubernetes
 
+import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.JBDefaultTreeCellRenderer
+import com.intellij.ui.PopupHandler
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.treeStructure.Tree
+import org.jboss.tools.intellij.kubernetes.tree.KubernetesTreeModel
 import java.awt.BorderLayout
 
 class KubernetesToolWindowFactory: ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        var contentFactory = ContentFactory.SERVICE.getInstance()
         val panel = JBPanel<JBPanel<*>>()
         panel.layout = BorderLayout()
+        val model = KubernetesTreeModel()
+        model.set
+        val tree = Tree(kubernetesTreeModel)
+        tree.cellRenderer = JBDefaultTreeCellRenderer()
+        PopupHandler.installPopupHandler(tree, "org.jboss.tools.intellij.kubernetes.tree", ActionPlaces.UNKNOWN)
+        panel.add(tree, BorderLayout.PAGE_START)
+        var contentFactory = ContentFactory.SERVICE.getInstance()
         toolWindow.contentManager.addContent(contentFactory.createContent(panel, "", false))
     }
 }
---
+++ src/main/kotlin/org/jboss/tools/intellij/kubernetes/tree/nodes/ErrorNode.kt
@@ -0,0 +1,6 @@
+package org.jboss.tools.intellij.kubernetes.tree.nodes
+
+import javax.swing.tree.DefaultMutableTreeNode
+
+class ErrorNode(message: String?): DefaultMutableTreeNode(message) {
+}
