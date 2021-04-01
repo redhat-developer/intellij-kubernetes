d7b7ee78598f0e237c394fec8e3a8454eb03196f
Andre Dietisheim <adietish@redhat.com>
2021-04-01 18:52:24 +0200

    hook schemaproviders in IJ (#31)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/kotlin/com/redhat/devtools/intellij/kubernetes/validation/KubernetesSchemaProvider.kt
+++ src/main/kotlin/com/redhat/devtools/intellij/kubernetes/validation/KubernetesSchemaProvider.kt
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Red Hat, Inc.
+ * Copyright (c) 2021 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -34,13 +34,11 @@ class KubernetesSchemaProvider(
 		return ApplicationManager.getApplication().runReadAction(
 			Computable<Boolean> {
 				val psiFile = PsiManager.getInstance(project!!).findFile(file)
-				return@Computable if (psiFile != null) {
-					val fileInfo = KubernetesTypeInfo.extractMeta(psiFile)
-					info == fileInfo
-					true
-				} else {
+				if (psiFile == null) {
 					false
 				}
+				val fileInfo = KubernetesTypeInfo.extractMeta(psiFile)
+				info == fileInfo
 			})
 	}
 
