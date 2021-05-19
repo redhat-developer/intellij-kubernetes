742ee487dfd2791d027109f1450b62c673faf0aa
Andre Dietisheim <adietish@redhat.com>
2021-05-19 14:53:19 +0200

    editor: change name when content is new resource
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/kotlin/com/redhat/devtools/intellij/kubernetes/editor/SaveListener.kt
+++ src/main/kotlin/com/redhat/devtools/intellij/kubernetes/editor/SaveListener.kt
@@ -10,7 +10,6 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.kubernetes.editor
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentSynchronizationVetoer
@@ -19,15 +18,11 @@ import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.progress.Progressive
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
-import com.redhat.devtools.intellij.kubernetes.model.ClusterResource
 import com.redhat.devtools.intellij.kubernetes.model.Notification
 import com.redhat.devtools.intellij.kubernetes.model.ResourceException
-import com.redhat.devtools.intellij.kubernetes.model.util.toMessage
 import com.redhat.devtools.intellij.kubernetes.model.util.trimWithEllipsis
 import io.fabric8.kubernetes.api.model.HasMetadata
-import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Listener that gets called when an editor is saved.
@@ -54,17 +49,14 @@ class SaveListener : FileDocumentSynchronizationVetoer() {
     }
 
     private fun saveToCluster(resource: HasMetadata, editor: FileEditor, project: Project) {
-        com.redhat.devtools.intellij.kubernetes.actions.run("Saving to cluster...", true,
-            Progressive {
-                try {
-                    ResourceEditor.saveToCluster(resource, editor, project)
-                } catch (e: ResourceException) {
-                    logger<SaveListener>().warn(e)
-                    Notification().error(
-                        "Could not save ${resource.kind} ${resource.metadata.name} to cluster",
-                    trimWithEllipsis(e.message, 300) ?: "")
-                }
-            })
+        try {
+            ResourceEditor.saveToCluster(resource, editor, project)
+        } catch (e: ResourceException) {
+            logger<SaveListener>().warn(e)
+            Notification().error(
+                "Could not save ${resource.kind} ${resource.metadata.name} to cluster",
+            trimWithEllipsis(e.message, 300) ?: "")
+        }
     }
 
     private fun getProjectAndEditor(file: VirtualFile): ProjectAndEditor? {
