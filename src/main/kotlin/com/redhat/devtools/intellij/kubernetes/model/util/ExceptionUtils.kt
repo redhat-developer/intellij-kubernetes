1d6757a8c97e6decba12e4f0fff571a86e29dbd0
Andre Dietisheim <adietish@redhat.com>
2022-05-05 15:06:45 +0200

    corrected exc handling && added unknown host exc in ResourceEditor#push, #pull, #update (#394)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/kotlin/com/redhat/devtools/intellij/kubernetes/model/util/ExceptionUtils.kt
+++ src/main/kotlin/com/redhat/devtools/intellij/kubernetes/model/util/ExceptionUtils.kt
@@ -1,10 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package com.redhat.devtools.intellij.kubernetes.model.util
 
-fun causeOrExceptionMessage(e: Throwable, prefix: String = ""): String {
-    val message = e.cause?.message ?: e.message
-    return if (message != null) {
-        prefix + message
+import io.fabric8.kubernetes.client.Config.DEFAULT_MASTER_URL
+import java.net.UnknownHostException
+
+fun toTitle(e: Throwable): String {
+    return noCurrentContextMessage(e) ?: exceptionOrCauseMessage(e)
+}
+
+fun toMessage(e: Throwable): String {
+    return noCurrentContextMessage(e) ?: causeOrExceptionMessage(e)
+}
+
+fun causeOrExceptionMessage(e: Throwable): String {
+    return e.cause?.message ?: e.message ?: "Error"
+}
+
+fun exceptionOrCauseMessage(e: Throwable): String {
+    return e.message ?: e.cause?.message ?: "Error"
+}
+
+fun noCurrentContextMessage(e: Throwable): String? {
+    return if (e is UnknownHostException
+        && e.message == DEFAULT_MASTER_URL) {
+        "No valid current context in kube config"
     } else {
-        ""
+        null
     }
 }
