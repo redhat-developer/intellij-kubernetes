79104d1b746d647b676cfa4b1c812ebd6d167a8a
Andre Dietisheim <adietish@redhat.com>
2022-07-01 19:27:11 +0200

    allow resource with generateName (#437)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/kotlin/com/redhat/devtools/intellij/kubernetes/model/resource/NonCachingSingleResourceOperator.kt
+++ src/main/kotlin/com/redhat/devtools/intellij/kubernetes/model/resource/NonCachingSingleResourceOperator.kt
@@ -10,10 +10,11 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.kubernetes.model.resource
 
-import com.redhat.devtools.intellij.kubernetes.model.resource.kubernetes.custom.CustomResourceDefinitionContextFactory
-import com.redhat.devtools.intellij.kubernetes.model.util.isCustomResource
+import com.redhat.devtools.intellij.kubernetes.model.util.hasGenerateName
+import com.redhat.devtools.intellij.kubernetes.model.util.hasName
 import io.fabric8.kubernetes.api.model.APIResource
 import io.fabric8.kubernetes.api.model.GenericKubernetesResource
+import io.fabric8.kubernetes.api.model.GenericKubernetesResourceList
 import io.fabric8.kubernetes.api.model.HasMetadata
 import io.fabric8.kubernetes.api.model.StatusBuilder
 import io.fabric8.kubernetes.client.KubernetesClient
@@ -21,8 +22,9 @@ import io.fabric8.kubernetes.client.KubernetesClientException
 import io.fabric8.kubernetes.client.Watch
 import io.fabric8.kubernetes.client.Watcher
 import io.fabric8.kubernetes.client.WatcherException
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation
 import io.fabric8.kubernetes.client.dsl.Resource
-import io.fabric8.kubernetes.client.dsl.VisitFromServerGetWatchDeleteRecreateWaitApplicable
+import io.fabric8.kubernetes.client.dsl.base.ResourceDefinitionContext
 import io.fabric8.kubernetes.client.utils.ApiVersionUtil
 import io.fabric8.kubernetes.client.utils.Serialization
 import java.net.HttpURLConnection
@@ -36,54 +38,113 @@ class NonCachingSingleResourceOperator(private val client: KubernetesClient, pri
 
     /**
      * Returns the latest version of the given resource from cluster. Returns `null` if none was found.
+     * Retrieves the resource on the cluster if the given resource has a [io.fabric8.kubernetes.api.model.ObjectMeta.name].
+     * Does nothing if this condition is not met.
      *
      * @param resource which is to be requested from cluster
      *
      * @return resource that was retrieved from cluster
      */
     fun get(resource: HasMetadata): HasMetadata? {
-        return if (isCustomResource(resource)) {
-            createCustomResourceOperation(toGenericKubernetesResource(resource), client)?.fromServer()?.get()
+        return if (hasName(resource)) {
+            val genericKubernetesResource = toGenericKubernetesResource(resource)
+            val op = createOperation(resource)
+            op.withName(genericKubernetesResource.metadata.name)
+                .fromServer()
+                .get()
         } else {
-            createLegacyResourceOperation(resource, client)?.fromServer()?.get()
+            null
         }
     }
 
     /**
      * Replaces the given resource on the cluster if it exists. Creates a new one if it doesn't.
+     * Creates or replaces the resource on the cluster if the given resource has a [io.fabric8.kubernetes.api.model.ObjectMeta.name]. or
+     * Creates a resource on the cluster if the given resource has a [io.fabric8.kubernetes.api.model.ObjectMeta.generateName].
+     * Does nothing if none of these conditions apply.
+     *
+     * **Note:** ["createOrReplace method doesn't support generateName for Jobs"](https://github.com/fabric8io/kubernetes-client/issues/2507#issuecomment-708418495)
+     * specifies that a `name` is required when using [io.fabric8.kubernetes.client.dsl.MixedOperation.createOrReplace].
+     * If there's no `name` you need to use [io.fabric8.kubernetes.client.dsl.MixedOperation.create].
      *
      * @param resource that shall be replaced on the cluster
      *
      * @return the resource that was created
      */
     fun replace(resource: HasMetadata): HasMetadata? {
-        return if (isCustomResource(resource)) {
-            val genericKubernetesResource = toGenericKubernetesResource(resource)
-            createCustomResourceOperation(genericKubernetesResource, client)
-                ?.createOrReplace(genericKubernetesResource)
+        val genericKubernetesResource = toGenericKubernetesResource(resource)
+        val op = createOperation(resource)
+
+        return if (hasName(genericKubernetesResource)) {
+            op.createOrReplace(genericKubernetesResource)
+        } else if (hasGenerateName(genericKubernetesResource)) {
+            op.create(genericKubernetesResource)
         } else {
-            createLegacyResourceOperation(resource, client)
-                ?.createOrReplace()
+            null
         }
     }
 
     /**
      * Creates a watch for the given resource.
+     * Watches the resource on the cluster only if the given resource has a [io.fabric8.kubernetes.api.model.ObjectMeta.name].
+     * Does nothing if this condition is not met.
      *
      * @param resource that shall be replaced on the cluster
      *
      * @return the resource that was created
      */
     fun watch(resource: HasMetadata, watcher: Watcher<HasMetadata>): Watch? {
-        return if (isCustomResource(resource)) {
-            createCustomResourceOperation(toGenericKubernetesResource(resource), client)
-                ?.watch(GenericKubernetesResourceWatcherAdapter(watcher))
+        val genericKubernetesResource = toGenericKubernetesResource(resource)
+        val op = createOperation(genericKubernetesResource)
+        return if (hasName(genericKubernetesResource)) {
+            op.watch(GenericKubernetesResourceWatcherAdapter(watcher))
         } else {
-            createLegacyResourceOperation(resource, client)
-                ?.watch(watcher)
+            null
         }
     }
 
+    private fun createOperation(resource: HasMetadata): NonNamespaceOperation<GenericKubernetesResource, GenericKubernetesResourceList, Resource<GenericKubernetesResource>> {
+        val context = createResourceDefinitionContext(resource)
+        val inNamespace = resourceOrClientNamespace(resource, client)
+        return if (context.isNamespaceScoped
+            && true == inNamespace?.isNotEmpty()
+        ) {
+            client.genericKubernetesResources(context)
+                .inNamespace(inNamespace)
+        } else {
+            client.genericKubernetesResources(context)
+        }
+    }
+
+    private fun createResourceDefinitionContext(resource: HasMetadata): ResourceDefinitionContext {
+        val kind = resource.kind
+        val group = ApiVersionUtil.trimGroupOrNull(resource.apiVersion)
+        val version = ApiVersionUtil.trimVersion(resource.apiVersion)
+        val apiResource = getAPIResource(resource)
+        return ResourceDefinitionContext.Builder()
+            .withKind(kind)
+            .withGroup(group)
+            .withVersion(version)
+            .withNamespaced(apiResource.namespaced)
+            .withPlural(apiResource.name)
+            .build()
+    }
+
+
+    private fun getAPIResource(resource: HasMetadata): APIResource {
+        val kind = resource.kind
+        val group = ApiVersionUtil.trimGroupOrNull(resource.apiVersion)
+        val version = ApiVersionUtil.trimVersion(resource.apiVersion)
+        return getAPIResource(kind, group, version)
+    }
+
+    /**
+     * Returns a [GenericKubernetesResource] for a given [HasMetadata].
+     * The given resource is returned as if it is a [GenericKubernetesResource].
+     *
+     * @param resource a HasMetadata to convert
+     * @return a GenericKubernetesResource
+     */
     private fun toGenericKubernetesResource(resource: HasMetadata): GenericKubernetesResource {
         return if (resource is GenericKubernetesResource) {
             resource
@@ -91,94 +152,6 @@ class NonCachingSingleResourceOperator(private val client: KubernetesClient, pri
             val yaml = Serialization.asYaml(resource)
             Serialization.unmarshal(yaml, GenericKubernetesResource::class.java)
         }
-    }
-
-    /**
-     * Returns an operation for custom resources given a [GenericKubernetesResource] and [KubernetesClient].
-     * This operation handles custom resources.
-     *
-     * @param resource the resource to handle
-     *
-     * @return the operation that's able to handle a custom resource
-     * @throws KubernetesClientException if an error occurs requesting or api discovery fails
-     *
-     * As for kubernetes-client 5.7.2 legacy resources are handled via [KubernetesClient.resource] while
-     * custom resources via [KubernetesClient.genericKubernetesResources].
-     * This is not needed starting with kubernetes-client >=5.9 which can handle
-     * legacy and custom resource via calling [KubernetesClient.resource].
-     */
-    private fun createCustomResourceOperation(
-        resource: GenericKubernetesResource, client: KubernetesClient
-    ): Resource<GenericKubernetesResource>? {
-        val kind = resource.kind
-        val group = ApiVersionUtil.trimGroupOrNull(resource.apiVersion)
-        val version = ApiVersionUtil.trimVersion(resource.apiVersion)
-        val apiResource = getAPIResource(resource)
-        val context = CustomResourceDefinitionContextFactory.create(
-            kind,
-            group,
-            version,
-            apiResource
-        ) ?: return null
-
-        return if (context.isNamespaceScoped) {
-            val namespace = resourceOrClientNamespace(resource, client)
-            client.genericKubernetesResources(context)
-                .inNamespace(namespace)
-                .withName(resource.metadata.name)
-        } else {
-            client.genericKubernetesResources(context)
-                .withName(resource.metadata.name)
-        }
-    }
-
-    private fun createKubernetesException(
-        kind: String?,
-        group: String?,
-        version: String?
-    ): KubernetesClientException {
-        return KubernetesClientException(
-            StatusBuilder()
-                .withMessage("Unsupported kind $kind in version $group/$version")
-                .withKind(kind)
-                .withApiVersion("$group/$version")
-                .withCode(HttpURLConnection.HTTP_UNSUPPORTED_TYPE)
-                .build()
-        )
-    }
-
-    /**
-     * Returns an operation for legacy resources given a [HasMetadata] and [KubernetesClient].
-     * This operation handles legacy/built-in resources.
-     *
-     * @param resource the resource to handle
-     * @return the operation that's able to handle legacy resources
-     *
-     * As for kubernetes-client 5.7.2 legacy resources are handled via [KubernetesClient.resource] while
-     * custom resources via [KubernetesClient.genericKubernetesResources].
-     * This is not needed starting with kubernetes-client >=5.9 which can handle
-     * legacy and custom resource when calling [KubernetesClient.resource].
-     */
-    private fun createLegacyResourceOperation(
-        resource: HasMetadata, client: KubernetesClient
-    ): VisitFromServerGetWatchDeleteRecreateWaitApplicable<HasMetadata>? {
-        if (resource is GenericKubernetesResource) {
-            return null
-        }
-        val apiResource = getAPIResource(resource)
-        return client.resource(resource).apply {
-            if (apiResource.namespaced) {
-                val namespace = resourceOrClientNamespace(resource, client)
-                this.inNamespace(namespace)
-            }
-        }
-    }
-
-    private fun getAPIResource(resource: HasMetadata): APIResource {
-        val kind = resource.kind
-        val group = ApiVersionUtil.trimGroupOrNull(resource.apiVersion)
-        val version = ApiVersionUtil.trimVersion(resource.apiVersion)
-        return getAPIResource(kind, group, version)
     }
 
     /**
@@ -196,12 +169,27 @@ class NonCachingSingleResourceOperator(private val client: KubernetesClient, pri
             ?: throw throw createKubernetesException(kind, group, version)
     }
 
-    private fun resourceOrClientNamespace(resource: HasMetadata?, client: KubernetesClient): String {
+    private fun resourceOrClientNamespace(resource: HasMetadata?, client: KubernetesClient): String? {
         return if (true == resource?.metadata?.namespace?.isNotBlank()) {
             resource.metadata.namespace
         } else {
             client.namespace
         }
+    }
+
+    private fun createKubernetesException(
+        kind: String?,
+        group: String?,
+        version: String?
+    ): KubernetesClientException {
+        return KubernetesClientException(
+            StatusBuilder()
+                .withMessage("Unsupported kind $kind in version $group/$version")
+                .withKind(kind)
+                .withApiVersion("$group/$version")
+                .withCode(HttpURLConnection.HTTP_UNSUPPORTED_TYPE)
+                .build()
+        )
     }
 
     private class GenericKubernetesResourceWatcherAdapter(private val watcher: Watcher<HasMetadata>): Watcher<GenericKubernetesResource> {
