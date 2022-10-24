cd126ab9fed4fc420022c4028f3e5fad188d19f1
Andre Dietisheim <adietish@redhat.com>
2022-10-24 15:52:58 +0200

    provided separate icons for ligh & dark theme (#469)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

---
+++ src/main/kotlin/icons/Icons.kt
@@ -0,0 +1,26 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object Icons {
+
+    @JvmField
+    val consolesToolwindow = loadIcon("icons/consoles-toolwindow.svg")
+    @JvmField
+    val download = loadIcon("icons/download.svg")
+    @JvmField
+    val upload = loadIcon("icons/upload.svg")
+    @JvmField
+    val diff = loadIcon("icons/diff.svg")
+    @JvmField
+    val removeClutter = loadIcon("icons/remove-clutter.svg")
+    @JvmField
+    val consoles = loadIcon("icons/consoles.svg")
+    @JvmField
+    val terminal = loadIcon("icons/terminal.svg")
+
+    private fun loadIcon(path: String): Icon {
+        return IconLoader.getIcon(path, Icons::class.java)
+    }
+}
