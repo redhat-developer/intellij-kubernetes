95eb93b18f764000a58756126181051bed938c94
Oleksii Korniienko <olkornii@redhat.com>
2024-05-14 11:11:40 +0200

    Reworked/refactored EditResourceTest.
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/org/jboss/tools/intellij/kubernetes/tests/EditResourceTest.java
+++ src/it/java/org/jboss/tools/intellij/kubernetes/tests/EditResourceTest.java
@@ -17,10 +17,8 @@ import com.intellij.remoterobot.utils.Keyboard;
 import org.jboss.tools.intellij.kubernetes.fixtures.mainIdeWindow.EditorsSplittersFixture;
 import org.jboss.tools.intellij.kubernetes.fixtures.menus.ActionToolbarMenu;
 
-import java.awt.event.KeyEvent;
 import java.util.List;
 
-import static com.intellij.remoterobot.search.locators.Locators.byXpath;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -28,6 +26,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class EditResourceTest extends AbstractKubernetesTest{
     public static void editResource(RemoteRobot robot, ComponentFixture kubernetesViewTree){
+        String yodaLabel = "yoda_label";
+        String yodaText = "yoda_text";
+
         openResourceContentList(new String[]{"Nodes"}, kubernetesViewTree);
         RemoteText selectedResource = getResourceByIdInParent("Nodes", 0, kubernetesViewTree);
         selectedResource.doubleClick();
@@ -35,19 +36,10 @@ public class EditResourceTest extends AbstractKubernetesTest{
         EditorsSplittersFixture editorSplitter = robot.find(EditorsSplittersFixture.class);
         String editorTitle = selectedResource.getText();
 
-        ComponentFixture textFixture = editorSplitter.getEditorTextFixture();
-        List<RemoteText> remote_text = textFixture.findAllText();
-        int labelsId = 0;
-        for (RemoteText actual_remote_text : remote_text){
-            if ("labels".equals(actual_remote_text.getText())){
-                break;
-            }
-            labelsId++;
-        }
-        RemoteText placeForNewLabel = remote_text.get(labelsId+2); // +1 because we need the next one, +1 because between every 2 real elements is space
+        RemoteText placeForNewLabel = getPlaceForNewLabel(editorSplitter);
         placeForNewLabel.click();
 
-        String text = "    yoda_label: \"yoda_text";
+        String text = "    " + yodaLabel +": \"" + yodaText;
         Keyboard my_keyboard = new Keyboard(robot);
 
         my_keyboard.enter(); // create empty line
@@ -64,11 +56,11 @@ public class EditResourceTest extends AbstractKubernetesTest{
 
         selectedResource.doubleClick();
         ComponentFixture textFixtureNew = editorSplitter.getEditorTextFixture();
-        scrollToVisible("yoda_label", robot);
+        scrollToVisible(yodaLabel, robot);
         List<RemoteText> remoteTextNew = textFixtureNew.findAllText();
         boolean labelExist = false;
         for (RemoteText actual_remote_text : remoteTextNew){
-            if (actual_remote_text.getText().contains("yoda_text")){
+            if (actual_remote_text.getText().contains(yodaText)){
                 labelExist = true;
                 break;
             }
@@ -77,13 +69,25 @@ public class EditResourceTest extends AbstractKubernetesTest{
         editorSplitter.closeEditor(editorTitle); // close editor
         hideClusterContent(kubernetesViewTree);
 
-        assertTrue(labelExist);
+        assertTrue(labelExist, "Label '" + yodaLabel + "' not found.");
     }
 
-    private static void scrollToVisible(String text, RemoteRobot robot) {
-        Keyboard myKeyboard = new Keyboard(robot);
-        myKeyboard.hotKey(KeyEvent.VK_CONTROL, KeyEvent.VK_F);
-        robot.find(ComponentFixture.class, byXpath("//div[@class='SearchTextArea']")).click();
-        myKeyboard.enterText(text);
+    private static RemoteText getPlaceForNewLabel (EditorsSplittersFixture editorSplitter) {
+        ComponentFixture textFixture = editorSplitter.getEditorTextFixture();
+        List<RemoteText> remote_text = textFixture.findAllText();
+
+        int labelsId = 0;
+        boolean labelsFound = false;
+        for (RemoteText actual_remote_text : remote_text){
+            if ("labels".equals(actual_remote_text.getText())){
+                labelsFound = true;
+                break;
+            }
+            labelsId++;
+        }
+
+        assertTrue(labelsFound, "Labels not found in resource.");
+
+        return remote_text.get(labelsId+2); // +1 because we need the next one, +1 because between every 2 real elements is space
     }
 }
